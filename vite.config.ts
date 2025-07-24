1a98e68d47ff2c600fa7349ea5d2b361415c89f8
Daniel Martin <danimp2002@gmail.com>
2025-07-24 10:35:36 +0200

    "Fix"

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tu-nombre-de-repo/', // IMPORTANT: Replace with your repository name!
+  base: '/Quantum-Computing-Mind-Map/',
 })
